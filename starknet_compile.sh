f23b08f18a98d3b3e86afcf3f7c97312ddf97004
0xNonCents <0xNonCents@gmail.com>
2022-05-24 13:07:19 -0500

    delete unused

--- starknet_compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-starknet-compile starknet_contract.cairo \
-    --disable_hint_validation \
-    --output build/starknet_contract_compiled.json \
-    --abi build/starknet_contract_abi.json
--- starknet_deploy.sh
+++
@@ -1,7 +0,0 @@
-export STARKNET_GATEWAY_URL=http://localhost:5000/
-export STARKNET_FEEDER_GATEWAY_URL=http://localhost:5000/
-#export STARKNET_CHAIN_ID=SN_GOERLI
-#export STARKNET_NETWORK_ID=hackathon-0
-# unset STARKNET_NETWORK
-
-starknet deploy --contract build/starknet_contract_compiled.json
